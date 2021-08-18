2e4dd72cfcd181d5cf939e86fd960ac872b9f3c9
kdw530 <kdw530@naver.com>
2021-08-18 23:01:03 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/kdw/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kdw/exam/demo/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.kdw.exam.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -12,18 +13,15 @@ import com.kdw.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title =?, `body` = ?;
-	public Article writeArticle(String title, String body);
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id")int id, @Param("title")String title, @Param("body")String body);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
+
+	public int getLastInsertId();
 }
